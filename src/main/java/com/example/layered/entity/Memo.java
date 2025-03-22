034dbaa2b9fc8c6bba645e2d6eafb5539be31142
HWKIM <pottq577@naver.com>
2025-03-22 15:48:28 +0900

    Refactor: 메모 생성 API 리팩토링 - Service 레이어 구현

--- src/main/java/com/example/layered/entity/Memo.java
+++ src/main/java/com/example/layered/entity/Memo.java
@@ -1,24 +1,34 @@
 package com.example.layered.entity;
 
-import com.example.layered.dto.MemoRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 public class Memo {
 
+  // 클래스 레벨에 @Setter 사용 시 전체 필드에 Setter 적용
+  // 필드 레벨에서 사용해야 원하는 필드의 @Setter를 적용할 수 있음
+  @Setter
   private Long id;
   private String title;
   private String contents;
 
-  public void update(MemoRequestDto dto) {
-    this.title = dto.getTitle();
-    this.contents = dto.getContents();
+  public Memo(String title, String contents) {
+    this.title = title;
+    this.contents = contents;
   }
 
-  public void updateTitle(MemoRequestDto dto) {
-    this.title = dto.getTitle();
+  // Dto 대신 기본 자료형으로 받아야 메소드의 유연성과 재사용성이 높아짐
+  // 테스트 코드에도 도움이 됨
+  public void update(String title, String contents) {
+    this.title = title;
+    this.contents = contents;
+  }
+
+  public void updateTitle(String title) {
+    this.title = title;
   }
 
 }
--- src/main/java/com/example/layered/service/MemoServiceImpl.java
+++ src/main/java/com/example/layered/service/MemoServiceImpl.java
@@ -1,11 +1,33 @@
 package com.example.layered.service;
 
+import com.example.layered.dto.MemoRequestDto;
+import com.example.layered.dto.MemoResponseDto;
+import com.example.layered.entity.Memo;
+import com.example.layered.repository.MemoRepository;
 import org.springframework.stereotype.Service;
 
-// @Component와 같음 즉, Spring Bean으로 등록된다.
-// 명시적으로 Service Layer를 나타냄
-// 비즈니스 로직 수행
+// 실제 비즈니스 로직 관련 처리는 서비스 레이어의 구현체에서 진행
 @Service
 public class MemoServiceImpl implements MemoService {
+
+  private final MemoRepository memoRepository;
+
+  // 생성자 주입
+  public MemoServiceImpl(MemoRepository memoRepository) {
+    this.memoRepository = memoRepository;
+  }
+
+  @Override
+  public MemoResponseDto saveMemo(MemoRequestDto dto) {
+
+    // 요청받은 데이터로 Memo 객체 생성
+    // 식별자 id는 없음 - Repository 영역이기 때문
+    Memo memo = new Memo(dto.getTitle(), dto.getContents());
+
+    // DB 저장
+    memoRepository.saveMemo(memo);
+
+    return null;
+  }
 
 }
