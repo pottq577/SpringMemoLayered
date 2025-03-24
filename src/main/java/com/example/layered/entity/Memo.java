4f0e4b0398c5bd0b4ac8e1e02ffd70da2c81b561
HWKIM <pottq577@naver.com>
2025-03-24 12:45:49 +0900

    Refactor: Memo 클래스의 Setter 제거, 불필요한 주석 삭제

--- src/main/java/com/example/layered/entity/Memo.java
+++ src/main/java/com/example/layered/entity/Memo.java
@@ -2,15 +2,11 @@ package com.example.layered.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 public class Memo {
 
-  // 클래스 레벨에 @Setter 사용 시 전체 필드에 Setter 적용
-  // 필드 레벨에서 사용해야 원하는 필드의 @Setter를 적용할 수 있음
-  @Setter
   private Long id;
   private String title;
   private String contents;
@@ -20,8 +16,6 @@ public class Memo {
     this.contents = contents;
   }
 
-  // Dto 대신 기본 자료형으로 받아야 메소드의 유연성과 재사용성이 높아짐
-  // 테스트 코드에도 도움이 됨
   public void update(String title, String contents) {
     this.title = title;
     this.contents = contents;
--- src/main/java/com/example/layered/service/MemoServiceImpl.java
+++ src/main/java/com/example/layered/service/MemoServiceImpl.java
@@ -9,13 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-// 실제 비즈니스 로직 관련 처리는 서비스 레이어의 구현체에서 진행
 @Service
 public class MemoServiceImpl implements MemoService {
 
   private final MemoRepository memoRepository;
 
-  // 생성자 주입
   public MemoServiceImpl(MemoRepository memoRepository) {
     this.memoRepository = memoRepository;
   }
@@ -23,11 +21,7 @@ public class MemoServiceImpl implements MemoService {
   @Override
   public MemoResponseDto saveMemo(MemoRequestDto dto) {
 
-    // 요청받은 데이터로 Memo 객체 생성
-    // 식별자 id는 없음 - Repository 영역이기 때문
     Memo memo = new Memo(dto.getTitle(), dto.getContents());
-
-    // DB 저장
     Memo savedMemo = memoRepository.saveMemo(memo);
 
     return new MemoResponseDto(savedMemo);
@@ -44,7 +38,6 @@ public class MemoServiceImpl implements MemoService {
 
     Memo memo = memoRepository.findMemoById(id);
 
-    // ResponseEntity<>(HttpStatus.NOT_FOUND) 사용 불가,
     if (memo == null) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "dose not exist id: " + id);
     }
